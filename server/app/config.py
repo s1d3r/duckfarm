770a6dd5f1085d6cd0ff8f11a8b999e4895a7f53
s1d3r <wowbackex@gmail.com>
2024-04-18 00:06:38 +0600

    v1.07

--- server/app/config.py
+++ server/app/config.py
@@ -11,7 +11,7 @@ CONFIG = {
     },
     # 'FLAG_FORMAT': r'CTF\.Moscow\{[a-zA-Z\.0-9_-]+\}',
     # 'FLAG_FORMAT': r'VolgaCTF{[\w-]*\.[\w-]*\.[\w-]*}',
-    'FLAG_FORMAT': CHANGE_FLAG_REGEX,
+    'FLAG_FORMAT': rCHANGE_FLAG_REGEX,
 
     # 'SYSTEM_PROTOCOL': 'ructf_http',
     # 'SYSTEM_URL': 'http://monitor.ructfe.org/flags',
